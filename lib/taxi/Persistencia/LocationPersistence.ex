9c136c43c48d776188dde8d9f3fadf794b56e0fd
lauraosorio102 <lauras.osoriom@uqvirtual.edu.co>
2025-11-04 15:23:04 -0500

    Se implementa el módulo LocationPersistence para los datos de las ubicaciónes
    Se implementa Taxi.LocationPersistence para gestionar la carga, la búsqueda y la comprobación de existencia de datos de ubicación almacenados en un archivo JSON.

---
+++ lib/taxi/Persistencia/LocationPersistence.ex
@@ -0,0 +1,25 @@
+defmodule Taxi.LocationPersistence do
+
+  alias Taxi.{Location, Persistence}
+
+  @locations_file "data/locations.json"
+
+  def load_all do
+    Persistence.read_json(@locations_file, Location)
+  end
+
+  def find_by_name(nombre) do
+    load_all()
+    |> Enum.find(&(&1.name == nombre))
+  end
+
+  @spec existe?(any()) :: none()
+  def existe?(nombre) do
+    case find_by_name(nombre) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+
+end
